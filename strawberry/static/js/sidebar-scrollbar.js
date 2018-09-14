2fe89eb9d0be13ccd907ef49d56062a8b8b44c12
yuki <yuki77x@googlemail.com>
2018-09-14 14:23:14 +0200

    Fix flickering of scrollbar when hidden
    window.onload is too late, page is already rendered causing the
    scrollbar to render and then be hidden
    DOMContentLoaded fixes that

--- strawberry/static/js/sidebar-scrollbar.js
+++ strawberry/static/js/sidebar-scrollbar.js
@@ -1,5 +1,5 @@
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function(e){
   var sidebar = document.getElementById('sidebar');
   var scrollbarwidth  = sidebar.offsetWidth - sidebar.clientWidth;
   sidebar.style.width = sidebar.offsetWidth + scrollbarwidth + "px";
-}
+});
